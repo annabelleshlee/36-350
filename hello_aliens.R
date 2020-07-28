3578546d26468f42f6bbfe5ec06b2c14a75217a7
annabelleshlee <annabelleshlee@hotmail.com>
2020-07-28 15:12:10 -0700

    Message to Aliens (R Project Q17)

---
+++ hello_aliens.R
@@ -0,0 +1,8 @@
+cat("Hello Aliens.  This is a message for any intelligent life. \n 
+    This message is from a planet we call Earth. We mean no harm. \n
+    We would like to exchange information and commuunicate. \n 
+    The life in this planet reaching out are called humans and we require \n 
+    specific conditions to maintain life. For example, we need a specific \n
+    atmosphere, a variety of sustenance, and a specific range of temperatures \n 
+    and pressures.  These requirements are naturally provided on Earth.  \n 
+    If this message is understood, please reply with any information about yourselves.\n")
